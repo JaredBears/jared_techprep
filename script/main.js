c24dc146139d4ebbce03b5b67eaea14575458e8d
Jared Bears <97640061+JaredBears@users.noreply.github.com>
2023-03-27 21:29:06 -0500

    Added Search Options (#6)
    Added options for zip code search
    Added options to convert from Celsius to Fahrenheit

--- script/main.js
+++ script/main.js
@@ -6,6 +6,47 @@ const api = {
 const searchbox = document.querySelector('.search-box');
 const locationButton = document.querySelector('.location-button');
 
+let city = true;
+
+let units = 'imperial';
+
+let temp = 0;
+let hiLo = [];
+let feelsLike = 0;
+
+let cityBox = document.querySelector('.location .city');
+let dateBox = document.querySelector('.location .date');
+let tempBox = document.querySelector('.current .temp');
+let weatherBox = document.querySelector('.current .weather');
+let hiLoBox = document.querySelector('.hi-low');
+let humidityBox = document.querySelector('.humidity');
+let feelsLikeBox = document.querySelector('.feels-like');
+
+const tempRadio = document.querySelectorAll('input[type=radio][name="temp-units"]');
+tempRadio.forEach((radio) => {
+  radio.addEventListener('change', (event) => {
+    if(radio.value == 'celsius'){
+      changeToCelsius();
+    } else {
+      changeToFahrenheit();
+    }
+  });
+});
+
+const locationRation = document.querySelectorAll('input[type=radio][name="city-zip"]');
+locationRation.forEach((radio) => {
+  radio.addEventListener('change', (event) => {
+    searchbox.value = '';
+    if(radio.value == 'city'){
+      searchbox.placeholder = 'Search for a city...';
+      city = true;
+    } else {
+      searchbox.placeholder = 'Search for a zip code...';
+      city = false;
+    }
+  });
+});
+
 searchbox.addEventListener('keypress', setQuery);
 locationButton.addEventListener('click', () => {
   searchbox.value='Current Location';
@@ -22,14 +63,20 @@ function setQuery(evt) {
 function getResults (query) {
   if(query == 'Current Location') {
     navigator.geolocation.getCurrentPosition(function(position) {
-      fetch(`${api.base}weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial&APPID=${api.key}`)
+      fetch(`${api.base}weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=${units}&APPID=${api.key}`)
         .then(weather => {
           return weather.json();
         }).then(displayResults);
     });
   }
+  else if (city) {
+    fetch(`${api.base}weather?q=${query}&units=${units}&APPID=${api.key}`)
+      .then(weather => {
+        return weather.json();
+      }).then(displayResults);
+  }
   else {
-    fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+    fetch(`${api.base}weather?zip=${query}&units=${units}&APPID=${api.key}`)
       .then(weather => {
         return weather.json();
       }).then(displayResults);
@@ -37,35 +84,35 @@ function getResults (query) {
 }
 
 function displayResults (weather) {
-  let city = document.querySelector('.location .city');
-  let date = document.querySelector('.location .date');
-  let temp = document.querySelector('.current .temp');
-  let weather_el = document.querySelector('.current .weather');
-  let hilow = document.querySelector('.hi-low');
-  let humidity = document.querySelector('.humidity');
-  let feelsLike = document.querySelector('.feels-like');
-
   if (weather.cod != 200) {
-    city.innerText = '';
-    date.innerText = '';
+    cityBox.innerText = '';
+    dateBox.innerText = '';
     temp.innerHTML = '';
-    weather_el.innerText = '';
-    hilow.innerText = '';
-    humidity = '';
-    feelsLike = '';
+    weatherBox.innerText = '';
+    hiLoBox.innerText = '';
+    humidityBox.innerText = '';
+    feelsLikeBox.innerText = '';
     alert("ERROR " + String(weather.cod) + ": " + weather.message);
     return;
   }
 
   let now = new Date();
-  city.innerText = `${weather.name}, ${weather.sys.country}`;
-  date.innerText = dateBuilder(now);
-  temp.innerHTML = `${Math.round(weather.main.temp)}°F / <span>(${Math.round(toCelsius(weather.main.temp))}°C)</span> `;
-  weather_el.innerText = weather.weather[0].main;
-  hilow.innerText = `${Math.round(weather.main.temp_min)}°F / ${Math.round(weather.main.temp_max)}°F \n \
-  (${Math.round(toCelsius(weather.main.temp_min))}°C / ${Math.round(toCelsius(weather.main.temp_max))}°C)`;
-  humidity.innerText = `Humidity: ${weather.main.humidity}%`;
-  feelsLike.innerText = `Feels Like: ${Math.round(weather.main.feels_like)}°F / ${Math.round(toCelsius(weather.main.feels_like))}°C`;
+  cityBox.innerText = `${weather.name}, ${weather.sys.country}`;
+  dateBox.innerText = dateBuilder(now);
+  weatherBox.innerText = weather.weather[0].main;
+  humidityBox.innerText = `Humidity: ${weather.main.humidity}%`;
+  if(units == 'metric') {
+    tempBox.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+    hiLoBox.innerText = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`;
+    feelsLikeBox.innerText = `Feels Like: ${Math.round(weather.main.feels_like)}°C`;
+  } else {
+    tempBox.innerHTML = `${Math.round(weather.main.temp)}<span>°F</span>`;
+    hiLoBox.innerText = `${Math.round(weather.main.temp_min)}°F / ${Math.round(weather.main.temp_max)}°F`;
+    feelsLikeBox.innerText = `Feels Like: ${Math.round(weather.main.feels_like)}°F`;
+  }
+  temp = weather.main.temp;
+  hiLo = [weather.main.temp_min, weather.main.temp_max];
+  feelsLike = weather.main.feels_like;
 }
 
 function dateBuilder (d) {
@@ -80,6 +127,26 @@ function dateBuilder (d) {
   return `${day} ${date} ${month} ${year}`;
 }
 
-function toCelsius(fahrenheit) {
-  return (5/9) * (fahrenheit-32);
+function changeToCelsius() {
+  if(units == "imperial"){
+    units = "metric";
+    temp = (temp - 32) * (5/9);
+    hiLo = [(hiLo[0] - 32) * (5/9), (hiLo[1] - 32) * (5/9)];
+    feelsLike = (feelsLike - 32) * (5/9);
+    tempBox.innerHTML = `${Math.round(temp)}<span>°C</span>`;
+    hiLoBox.innerText = `${Math.round(hiLo[0])}°C / ${Math.round(hiLo[1])}°C`;
+    feelsLikeBox.innerText = `Feels Like: ${Math.round(feelsLike)}°C`;
+  }
+}
+
+function changeToFahrenheit() {
+  if(units == "metric"){
+    units = "imperial";
+    temp = (temp * (9/5)) + 32;
+    hiLo = [(hiLo[0] * (9/5)) + 32, (hiLo[1] * (9/5)) + 32];
+    feelsLike = (feelsLike * (9/5)) + 32;
+    tempBox.innerHTML = `${Math.round(temp)}<span>°F</span>`;
+    hiLoBox.innerText = `${Math.round(hiLo[0])}°F / ${Math.round(hiLo[1])}°F`;
+    feelsLikeBox.innerText = `Feels Like: ${Math.round(feelsLike)}°F`;
+  }
 }
